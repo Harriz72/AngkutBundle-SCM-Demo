ce52701c5f45cdf85c797304b4cbe6bcf8febb2f
Harriz72 <122156749+Harriz72@users.noreply.github.com>
2025-01-07 21:51:54 +0800

    Optimize database query.

--- cart.php
+++ cart.php
@@ -2,3 +2,6 @@ echo "Resolved null value error in Add to Cart API" >> commit-log.txt
 
 
 echo "Added input validation for product IDs in Add to Cart" >> commit-log.txt
+
+
+echo "Optimized database query for adding items to the cart" >> commit-log.txt
